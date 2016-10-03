d7a64d2c66e83760c7c3dd2145b77c64eba0e572
Srđan Vukić <spookycorp@gmx.com>
2016-10-03 21:46:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php require_once('docs/'); exit;
+<?php 
+ob_start(); 
+require_once('docs/index.html'); 
+$out = ob_get_clean();
+header('Content-type: text/html; charset=utf-8');
+header('X-Content-Generator: github-templates');
+echo($out);
+exit;
