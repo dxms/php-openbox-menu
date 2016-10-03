c59c67ea20e40686ad5ba53ff991268544e2f492
Srđan Vukić <spookycorp@gmx.com>
2016-10-03 21:56:31 +0200

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-ob_start(); 
-require_once('docs/index.html'); 
-$out = ob_get_clean();
-header('Content-type: text/html; charset=utf-8');
-header('X-Content-Generator: github-templates');
-echo($out);
-exit;
